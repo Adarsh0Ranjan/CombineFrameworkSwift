ed29ec76a4b5af1075c52b6da11eca2f5a4d63bf
Adarsh Ranjan <adarsh0ranjan@gmail.com>
2025-09-15 20:18:09 +0530

    Initial Commit

---
+++ CombineSwift/CombineSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineSwiftApp.swift
+//  CombineSwift
+//
+//  Created by Adarsh Ranjan on 15/09/25.
+//
+
+import SwiftUI
+
+@main
+struct CombineSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
